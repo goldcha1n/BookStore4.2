dfc2b172bc8f43514dfe438f22a73d3fddf62f34
Евгений Щербина <wotccp@gmail.com>
2023-05-10 10:24:40 +0300

    static + bootstrap

--- config/settings.py
+++ config/settings.py
@@ -39,8 +39,6 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'django.contrib.sites',
     # Third-party
-    "crispy_forms"
-    "crispy_bootstrap5",
     'allauth',
     'allauth.account',
     # Local
@@ -129,6 +127,9 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
 STATIC_URL = 'static/'
+STATICFILES_DIRS = [BASE_DIR / "static"]  # new
+STATIC_ROOT = BASE_DIR / "staticfiles"  # new
+STATICFILES_STORAGE = "django.contrib.staticfiles.storage.StaticFilesStorage"  # new
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
